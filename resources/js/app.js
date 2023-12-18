e133d1d29b75df6d84d507a99f667df6f087bff5
lanangksma <kusumalanang1@gmail.com>
2023-12-18 17:22:45 +0700

    Feat: Membuat tampilan depan #2, Pada : 2023-12-18 17:22:45

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,18 @@
 import './bootstrap';
 
+import Swiper from 'swiper/bundle';
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 0,
+    autoplay: {
+        delay: 5000,
+    },
+    loop: true,
+});
