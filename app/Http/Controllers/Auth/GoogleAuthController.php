47e24ffc1c3e14358d6838ebbe130098fe5409bf
lanangksma <kusumalanang1@gmail.com>
2023-12-28 02:24:02 +0700

    Fix Login with google, Pada : 2023-12-28 02:24:02

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
 
 class GoogleAuthController extends Controller
@@ -14,18 +18,35 @@ class GoogleAuthController extends Controller
 
     public function handleGoogleCallback()
     {
-        $user = Socialite::driver('google')->user();
+        try {
+            // Ambil data user dari Google setelah mereka memberikan izin
+            $googleUser = Socialite::driver('google')->user();
 
-        // Lakukan penanganan login atau registrasi dengan data user dari Google.
-        // Misalnya:
-        // $existingUser = User::where('email', $user->email)->first();
-        // if ($existingUser) {
-        //     // Lakukan proses login
-        // } else {
-        //     // Lakukan proses registrasi untuk user baru
-        // }
+            // Cari apakah ada user dengan email dari data Google di database
+            $existingUser = User::where('email', $googleUser->getEmail())->first();
 
-        // Setelah proses login atau registrasi, arahkan pengguna ke halaman beranda atau halaman lainnya.
-        return redirect()->route('home');
+            if ($existingUser) {
+                // Jika user sudah ada, lakukan proses login
+                Auth::login($existingUser);
+                return redirect()->route('dashboard'); // Ganti dengan rute setelah login
+            } else {
+                // Jika user belum ada, lakukan proses registrasi untuk user baru
+                $newUser = User::create([
+                    'name' => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                    'password' => bcrypt(Str::random(16)),
+                ]);
+
+                // Lakukan login untuk user baru
+                Auth::login($newUser);
+                return redirect()->route('dashboard'); // Ganti dengan rute setelah login
+            }
+        } catch (\Exception $e) {
+            // Tangani pengecualian di sini
+            // Misalnya, tampilkan pesan error atau log pesan kesalahan
+            Log::error('Google Authentication Error: ' . $e->getMessage());
+            return redirect()->route('login')->withErrors(['msg' => 'Terjadi kesalahan saat autentikasi dengan Google']);
+        }
     }
+
 }
